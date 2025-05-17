6e84974105bcb6ff0280638e12bfded490d92cc0
Luis Sanchez <luis.udec.sanchez@gmail.com>
2025-05-17 15:29:52 -0500

    feat: Add react-gtm-module for Google Tag Manager integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const tagManagerArgs = {
+	gtmId: 'AW-11457519495',
+};
+TagManager.initialize(tagManagerArgs);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
